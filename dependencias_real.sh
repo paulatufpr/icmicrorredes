9dbecf51ee346620a14d112f71efd24ca621ca09
paulatufpr <113630543+paulatufpr@users.noreply.github.com>
2023-04-12 01:09:44 -0300

    Update and rename dependencias.sh to dependencias_real.sh

--- dependencias_real.sh
+++ dependencias_real.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "instalando dependencias do linux(ubuntu)
+echo "instalando dependencias do linux(ubuntu)"
 sudo apt install can-utils -y
-
+sudo apt install net-tools
 echo "dependencias python" 
 sudo pip install python-can 
 
 echo "ligando interface CAN FISICA" 
-sudo modprobe can
 sudo ip link add dev can0 type can bitrate 500000
 sudo ip link set up can0
+echo "Checagem se a rede realmente está ligada"
+ifconfig can0
