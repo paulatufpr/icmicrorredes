f409cc4102e79d1636f0762292320b4d6f99b76a
paulatufpr <113630543+paulatufpr@users.noreply.github.com>
2023-04-12 00:16:38 -0300

    Update dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -2,12 +2,11 @@
 
 echo "instalando dependencias do linux(ubuntu)
 sudo apt install can-utils -y
-sudo apt install modprobe -y
 
 echo "dependencias python" 
 sudo pip install python-can 
 
 echo "ligando interface CAN FISICA" 
-sudo modprobe can0
+sudo modprobe can
 sudo ip link add dev can0 type can bitrate 500000
 sudo ip link set up can0
